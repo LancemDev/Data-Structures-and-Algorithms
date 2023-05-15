5b1895c5019e4ca505f061de0832170accba2bbe
LancemDev <munyaolance1@gmail.com>
2023-05-15 11:34:30 +0300

    Stacks
    Stack implementation in Java

---
+++ Stacks/Stack.java
@@ -0,0 +1,77 @@
+package Stacks;
+
+public class Stack {
+  private int arr[];
+  private int top;
+  private int capacity;
+
+  // Creating a stack
+  Stack(int size) {
+    //The array is initialized to the size of the stack which is the parameter of the Stack
+    arr = new int[size];
+    //The capacity is the size
+    capacity = size;
+    top = -1;
+  }
+
+  // Add elements into stack
+  public void push(int x) {
+    //The isFull() method is implemented later in the program
+    if (isFull()) {
+      //The System.exit(1) method terminates the program
+      System.out.println("OverFlow\nProgram Terminated\n");
+      System.exit(1);
+    }
+
+    System.out.println("Inserting " + x);
+    arr[++top] = x;
+  }
+
+  // Remove element from stack
+  public int pop() {
+    if (isEmpty()) {
+      System.out.println("STACK EMPTY");
+      System.exit(1);
+    }
+    return arr[top--];
+  }
+
+  // Utility function to return the size of the stack
+  public int size() {
+    return top + 1;
+  }
+
+  // Check if the stack is empty
+  public Boolean isEmpty() {
+    return top == -1;
+  }
+
+  // Check if the stack is full
+  public Boolean isFull() {
+    return top == capacity - 1;
+  }
+
+  public void printStack() {
+    for (int i = 0; i <= top; i++) {
+      System.out.println(arr[i]);
+    }
+  }
+
+  public static void main(String[] args) {
+    Stack stack = new Stack(5);
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    stack.push(4);
+
+    System.out.println("Current Stack: \n");
+    stack.printStack();
+
+    stack.pop();
+    System.out.println("\nAfter popping out");
+
+    stack.printStack();
+
+  }
+}
